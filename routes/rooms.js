66a7f4698784ef6339203a303d16bf0f31c47bd8
Ryan Fredlund <rfredlund13@gmail.com>
2020-11-21 20:07:33 -0600

    Using express-validator, doing initial validation for getting room page

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const validator = require('express-validator');
+
+// Validations
+const validateRoomId = validator.param('roomId').trim().isUUID(4);
 
 /* GET planning rooms. */
-router.get('/:roomId', function(req, res, next) {
-  res.send('respond with a room. ' + req.params.roomId);
+router.get('/:roomId', [ validateRoomId ], (req, res, next) => {
+  const errors = validator.validationResult(req);
+  if (!errors.isEmpty()) {
+    console.error("Validation error: " + JSON.stringify(errors.array()));
+    return res.status(400).render('error', { "message": "Room not found." });
+  }
+
+  res.status(200).render('room', { 'roomId': req.params.roomId });
 });
 
 module.exports = router;
