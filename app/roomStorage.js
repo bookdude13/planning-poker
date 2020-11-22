47c20dcc75a6f914f9f3fca343b40fbc285bd7d8
Ryan Fredlund <rfredlund13@gmail.com>
2020-11-22 15:34:01 -0600

    Using promises for db queries

--- app/roomStorage.js
+++ app/roomStorage.js
@@ -1,14 +1,9 @@
 let storage = require('./storage');
 
-function getRooms() {
-  let pool = storage.getPool();
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-    connection.query('SELECT 1+1', (err, rows) => {
-      if (err) throw err;
-      console.log(rows);
-    })
-  })
+async function getRooms() {
+  let rooms = await storage.query("SELECT * FROM room", {});
+  console.log(rooms);
+  return rooms;
 }
 
 module.exports = {
--- app/storage.js
+++ app/storage.js
@@ -2,15 +2,42 @@ const config = require('../config');
 const mysql = require('mysql');
 
 var pool;
-module.exports = {
-    getPool: function() {
-        if (pool) return pool;
-        pool = mysql.createPool({
-            host: config.db.host,
-            user: config.db.username,
-            password: config.db.password,
-            database: 'planningpoker'
-        });
-        return pool;
-    }
+function getPool() {
+    if (pool) return pool;
+    pool = mysql.createPool({
+        host: config.db.host,
+        user: config.db.username,
+        password: config.db.password,
+        database: 'planningpoker'
+    });
+    return pool;
 }
+
+async function getConnection() {
+    return new Promise((resolve, reject) => {
+        let pool = getPool();
+        pool.getConnection((err, connection) => {
+            if (err) reject(err);
+            resolve(connection);
+        });
+    });
+}
+
+async function query(query, args) {
+    return new Promise((resolve, reject) => {
+        getConnection()
+            .then((connection) => {
+                connection.query(query, args, (err, result) => {
+                    if (err) reject(err);
+                    resolve(result);
+                });
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+module.exports = {
+    query
+};
